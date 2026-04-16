d11ff877ebb9cc45462c6db9c902487f7ec83b3a
arieljordz <arieljordz@gmail.com>
2026-04-16 21:50:49 +0800

    invoice-inquiry-search-export

---
+++ CbsAp/CbsAp.Application/DTOs/InvoiceInquiry/InvoiceInquirySearchDto.cs
@@ -0,0 +1,17 @@
+﻿using CbsAp.Domain.Enums;
+
+namespace CbsAp.Application.DTOs.InvoiceInquiry
+{
+    public class InvoiceInquirySearchDto
+    {
+        public string? SupplierName { get; set; }
+        public string? InvoiceNumber { get; set; }
+        public string? PONumber { get; set; }
+        public int? Status { get; set; }
+        public DateTimeOffset? ScanDateFrom { get; set; }
+        public DateTimeOffset? ScanDateTo { get; set; }
+        public DateTimeOffset? InvoiceDateFrom { get; set; }
+        public DateTimeOffset? InvoiceDateTo { get; set; }
+
+    }
+}
---
+++ CbsAp/CbsAp.Application/Features/InvoiceInquiry/Queries/Reports/ExportInvoiceInquiryQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿using CbsAp.Application.Abstractions.Messaging;
+using CbsAp.Application.Abstractions.Services.InvoiceInquiry;
+using CbsAp.Application.Abstractions.Services.Reports;
+using CbsAp.Application.Configurations.constants;
+using CbsAp.Application.Shared.ResultPatten;
+using CbsAp.Domain.Enums;
+
+namespace CbsAp.Application.Features.InvoiceInquiry.Queries.Reports
+{
+    public class ExportInvoiceInquiryQueryHandler : IQueryHandler<ExportInvoiceInquiryQuery, ResponseResult<byte[]>>
+    {
+        private readonly IInvoiceInquiryService _invoiceInquiryService;
+        private readonly IExcelService _excelService;
+
+        public ExportInvoiceInquiryQueryHandler(IInvoiceInquiryService invoiceInquiryService, IExcelService excelService)
+        {
+            _invoiceInquiryService = invoiceInquiryService;
+            _excelService = excelService;
+        }
+
+        public async Task<ResponseResult<byte[]>> Handle(ExportInvoiceInquiryQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _invoiceInquiryService.ExportInvoiceInquiryToExcel(
+                request.SupplierName,
+                request.InvoiceNumber, 
+                request.PONumber, 
+                request.Status, 
+                request.ScanDateFrom,
+                request.ScanDateTo, 
+                request.InvoiceDateFrom,
+                request.InvoiceDateTo, 
+                cancellationToken);
+
+            if (!result.Any() || result == null)
+            {
+                return ResponseResult<byte[]>.NotFound(MessageConstants.Message("InvoiceInquiry", MessageOperationType.NotFound));
+            }
+
+            var excelBytes = await Task.Run(() => _excelService.GenerateExcel(result, "InvoiceInquiry"));
+
+            return ResponseResult<byte[]>.Success(excelBytes, "Export excel data");
+        }
+    }
+}

4f98aae968a3d0e19277e84d71c881bf557f8a45
arieljordz <arieljordz@gmail.com>
2026-06-02 00:23:38 +0800

    refactor-advance-search-parameters-query

--- CbsAp/CbsAp.Application/Features/Invoicing/InvActions/Queries/ExceptionsSearch/ExceptionsSearchQueryHandler.cs
+++ CbsAp/CbsAp.Application/Features/Invoicing/InvActions/Queries/ExceptionsSearch/ExceptionsSearchQueryHandler.cs
@@ -28,6 +28,38 @@ namespace CbsAp.Application.Features.Invoicing.InvActions.Queries.ExceptionsSear
                 request.SortField,
                 request.SortOrder,
                 request.RoleId,
+                // Supplier Info
+                request.PaymentTerm,
+                request.SupplierNo,
+                request.SuppABN,
+                request.SuppBankAccount,
+
+                // Invoice Detail
+                request.EntityProfileID,
+                request.GrNo,
+                request.StartInvoiceDate,
+                request.EndInvoiceDate,
+                request.StartDueDate,
+                request.EndDueDate,
+                request.DaystillDue,
+
+                // Amounts
+                request.NetAmount,
+                request.TaxCodeID,
+                request.TaxAmount,
+                request.Currency,
+                request.TotalAmount,
+
+                // Routing Flow
+                request.InvRoutingFlowName,
+                request.NextRole,
+                request.Keyword,
+
+                // Transaction Info
+                request.MapID,
+                request.StartScanDate,
+                request.EndScanDate,
+                request.InvoiceID,
                 cancellationToken
                 );
 
